97c7b8c4477f638b263a50be56b48f771f55c4a7
kazuki19992 <kushida98@gmail.com>
2022-05-19 00:34:05 +0900

    fix: 表示が崩れていたので修正

--- App.tsx
+++ App.tsx
@@ -4,12 +4,27 @@ import tw from 'twrnc';
 import HomeCard from './components/HomeCard';
 
 export default function App() {
+  const cardData = {
+    title: "ここに講義名がはいるよ",
+    start: "13:00",
+    end: "14:30",
+    place: "ここに開講場所が入るよ",
+    color: "lime",
+  }
+  const cardData2 = {
+    title: "ネットワーク管理技術および演習",
+    start: "14:40",
+    end: "16:10",
+    place: "70-512Lab",
+    color: "cyan",
+  }
   return (
     <View style={styles.container}>
 
       {/* カードデザイン案 */}
-      <HomeCard />
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <HomeCard title={cardData.title} start={cardData.start} end={cardData.end} place={cardData.place} color={cardData.color} />
+      <HomeCard title={cardData2.title} start={cardData2.start} end={cardData2.end} place={cardData2.place} color={cardData2.color} />
+      <Text>これはカード表示の例だよ</Text>
       <StatusBar style="auto" />
     </View>
   );
--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -1,41 +1,58 @@
 import tw from 'twrnc';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
-const HomeCard = () => {
+interface Task {
+  title: string,
+  color: string,
+};
+
+interface Props {
+  title: string,
+  start: string,
+  end: string,
+  place: string,
+  color: string,
+  tasks?: Task[],
+}
+
+const HomeCard = (props: Props) => {
   return (
-    <View style={tw.style('shadow-xl', 'rounded-xl', 'mb-5', 'p-2', 'w-4/5')}>
-        <div style={tw.style('flex', 'justify-between', 'w-full', 'flex-row', 'items-center')}>
-          <Text style={tw.style('font-bold', 'pr-1')}>あああ</Text>
-          <div style={tw.style('bg-cyan-500', 'h-5', 'grow', 'rounded')}></div>
-        </div>
-        <Text style={tw.style('font-bold', 'text-2xl')}>aaaaaaaa</Text>
-        <Text style={tw.style('text-sm')}>12:34 - 23:45 Lab-01</Text>
-        <hr style={tw.style('w-full')}/>
-        <div style={tw.style('flex', 'justify-between', 'space-x-1')}>
-          <div style={tw.style('w-1/2', 'mr-1')}>
+    <View style={tw.style('shadow-xl', 'rounded-xl', 'mb-5', 'p-2', 'w-4/5', 'bg-white')}>
+        <View style={tw.style('flex', 'justify-between', 'w-full', 'flex-row', 'items-center')}>
+          <Text style={tw.style('font-bold', 'pr-1')}>講義中……</Text>
+          <View style={tw.style(`bg-${props.color}-500`, 'h-4', 'grow', 'rounded')}></View>
+        </View>
+        <Text style={tw.style('font-bold', 'text-2xl', 'mt-2')}>{props.title}</Text>
+        <Text style={tw.style('text-sm')}>{props.start} - {props.end} <Text style={tw.style('italic')}>{props.place}</Text></Text>
+
+        {/* hr */}
+        <View style={tw.style('border-b', 'my-2', 'border-gray-500')} />
+
+        <View style={tw.style('flex', 'justify-between', 'flex-row')}>
+          <View style={tw.style('w-1/2', 'mr-1')}>
             {/* 予定 */}
-            <div style={tw.style('flex', 'items-center', 'px-1')}>
-              <div style={tw.style('w-2', 'h-2', 'rounded-full', 'bg-red-500', 'mr-1')} />
+            <View style={tw.style('flex', 'items-center', 'px-1', 'flex-row')}>
+              <View style={tw.style('w-2', 'h-2', 'rounded-full', 'bg-red-500', 'mr-1')} />
               <Text style={tw.style('font-bold', 'text-red-500', 'text-sm')}>超重要なタスク</Text>
-            </div>
-            <div style={tw.style('flex', 'items-center', 'px-1')}>
-              <div style={tw.style('w-2', 'h-2', 'rounded-full', 'bg-amber-500', 'mr-1')} />
+            </View>
+            <View style={tw.style('flex', 'items-center', 'px-1', 'flex-row')}>
+              <View style={tw.style('w-2', 'h-2', 'rounded-full', 'bg-amber-500', 'mr-1')} />
               <Text style={tw.style('font-bold', 'text-amber-500', 'text-sm')}>重要なタスク</Text>
-            </div>
-            <div style={tw.style('flex', 'items-center', 'px-1')}>
-              <div style={tw.style('w-2', 'h-2', 'rounded-full', 'bg-green-500', 'mr-1')} />
+            </View>
+            <View style={tw.style('flex', 'items-center', 'px-1', 'flex-row')}>
+              <View style={tw.style('w-2', 'h-2', 'rounded-full', 'bg-green-500', 'mr-1')} />
               <Text style={tw.style('font-bold', 'text-green-500', 'text-sm')}>タスク</Text>
-            </div>
-            <div style={tw.style('flex', 'items-center', 'rounded-full', 'bg-gray-200', 'px-1')}>
-              <div style={tw.style('w-2', 'h-2', 'rounded-full', 'bg-gray-700', 'mr-1')} />
+            </View>
+            <View style={tw.style('flex', 'items-center', 'rounded-full', 'bg-gray-200', 'px-1', 'flex-row')}>
+              <View style={tw.style('w-2', 'h-2', 'rounded-full', 'bg-gray-700', 'mr-1')} />
               <Text style={tw.style('font-bold', 'text-gray-700', 'text-sm')}>他1件のタスク</Text>
-            </div>
-          </div>
+            </View>
+          </View>
           {/* 予定追加ボタン */}
-          <div style={tw.style('bg-gray-200', 'text-gray-700', 'w-1/2', 'flex', 'justify-center', 'items-center', 'rounded')}>
+          <View style={tw.style('bg-gray-200', 'text-gray-700', 'grow', 'flex', 'justify-center', 'items-center', 'rounded')}>
             <Text>タスクの追加</Text>
-          </div>
-        </div>
+          </View>
+        </View>
       </View>
   )
 }
